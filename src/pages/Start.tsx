de13bbb3982425f92171de965da430ab1746c042
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-09-02 18:37:55 +0200

    feat: make Start FileArea invisible and add FileAreaSection

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,16 +1,17 @@
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { toast } from "sonner";
+
+import FileArea, { FileAreaSection } from "@/components/FileArea/index.tsx";
 import Layout from "@/components/Layout/index.tsx";
-import FileArea from "@/components/FileArea/index.tsx";
+import { Spinner } from "@/components/Spinner/index.tsx";
 import {
   Heading3,
   InlineCode,
   Lead,
   Muted,
 } from "@/components/Typography/index.tsx";
-import { useTranslation } from "react-i18next";
-import { useCallback } from "react";
-import { Spinner } from "@/components/Spinner/index.tsx";
 import useFontContext from "@/hooks/useFontContext.tsx";
-import { toast } from "sonner";
 
 const Start = () => {
   const { t } = useTranslation(["app", "start"]);
@@ -34,29 +35,32 @@ const Start = () => {
     <Layout fullscreen>
       <FileArea
         className="flex h-full items-center justify-center"
+        invisible
         fileTypes={fileTypes.split(", ")}
         onChange={handleFileAreaChange}
         onFileTypeOutOfRange={handleFileTypeOutOfRange}
       >
-        <div className="m-4 flex h-full flex-col items-center justify-center">
-          <div>
-            <Heading3>{t("app:name")}</Heading3>
-            <Lead>
-              {dropToStart.split(fileTypes)[0]}
-              <InlineCode>{fileTypes}</InlineCode>
-              {dropToStart.split(fileTypes)[1]}
-            </Lead>
-            {font && (
-              <div className="mt-6">
-                <Muted>
-                  <Spinner className="mr-2 inline" />
-                  <span className="align-middle">
-                    {`${t("start:evaluating")} ${font?.name}`}
-                  </span>
-                </Muted>
-              </div>
-            )}
-          </div>
+        <div className="flex h-full flex-col items-center justify-center">
+          <FileAreaSection>
+            <div className="p-4">
+              <Heading3>{t("app:name")}</Heading3>
+              <Lead>
+                {dropToStart.split(fileTypes)[0]}
+                <InlineCode>{fileTypes}</InlineCode>
+                {dropToStart.split(fileTypes)[1]}
+              </Lead>
+              {font && (
+                <div className="mt-6">
+                  <Muted>
+                    <Spinner className="mr-2 inline" />
+                    <span className="align-middle">
+                      {`${t("start:evaluating")} ${font?.name}`}
+                    </span>
+                  </Muted>
+                </div>
+              )}
+            </div>
+          </FileAreaSection>
         </div>
       </FileArea>
     </Layout>
