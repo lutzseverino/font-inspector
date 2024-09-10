909342174f3b9363fd331e824f3dae7e82960ea4
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-09-10 17:08:25 +0200

    fix: use ternaries to apply Layout style correctly

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,11 +11,13 @@ const Layout: FunctionComponent<LayoutProps> = ({
   children,
 }) => {
   return (
-    <div className={`flex flex-col min-h-svh ${fullscreen && "h-svh"} `}>
+    <div
+      className={`flex flex-col min-h-svh ${fullscreen ? "h-svh" : "h-auto"} `}
+    >
       {!hideNavbar && <Navbar {...navbarProps} />}
       <div className="grow relative">
         <div
-          className={`absolute bottom-0 left-0 right-0 top-0 ${!disablePadding && "p-4"}`}
+          className={`absolute bottom-0 left-0 right-0 top-0 ${disablePadding ? "p-0" : "p-4"} `}
         >
           {children}
         </div>
