584e6160f59f3a048f440883eef39ecf1a75ec4a
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-09-12 01:12:19 +0200

    feat: add search to glyph page

--- src/pages/Glyphs/index.tsx
+++ src/pages/Glyphs/index.tsx
@@ -1,22 +1,29 @@
 import GlyphsPagination from "./Pagination";
 import {
+  ChangeEventHandler,
   FunctionComponent,
   ReactElement,
   useCallback,
+  useEffect,
   useMemo,
   useState,
 } from "react";
+import { useTranslation } from "react-i18next";
 
 import Layout from "@/components/base/Layout";
 import Glyph from "@/components/ui/Glyph";
+import { Input } from "@/components/ui/input.tsx";
 import FontGuard from "@/components/utils/FontGuard/index.tsx";
 import useFiFont from "@/hooks/useFiFont.tsx";
 
 const Glyphs: FunctionComponent = () => {
+  const { t } = useTranslation("pages/glyphs");
   const { fiFont } = useFiFont();
 
-  const glyphsPerPage = 36;
+  const [filteredGlyphs, setFilteredGlyphs] = useState<ReactElement[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+
+  const glyphsPerPage = 30;
 
   const glyphs: ReactElement[] = useMemo(
     () =>
@@ -26,34 +33,53 @@ const Glyphs: FunctionComponent = () => {
     [fiFont],
   );
 
-  const currentGlyphs = useMemo(() => {
-    return glyphs.slice(
+  useEffect(() => {
+    setFilteredGlyphs(glyphs);
+  }, [glyphs]);
+
+  const displayedGlyphs = useMemo(() => {
+    return filteredGlyphs.slice(
       (currentPage - 1) * glyphsPerPage,
       currentPage * glyphsPerPage,
     );
-  }, [currentPage, glyphs, glyphsPerPage]);
+  }, [currentPage, filteredGlyphs, glyphsPerPage]);
 
   const totalPages = useMemo(() => {
-    return Math.ceil(glyphs.length / glyphsPerPage);
-  }, [glyphs, glyphsPerPage]);
+    return Math.ceil(filteredGlyphs.length / glyphsPerPage);
+  }, [filteredGlyphs, glyphsPerPage]);
 
-  const handlePageChange = useCallback((pageNumber: number) => {
+  const handleChangePage = useCallback((pageNumber: number) => {
     setCurrentPage(pageNumber);
   }, []);
+
+  const handleSearch: ChangeEventHandler<HTMLInputElement> = useCallback(
+    (event) => {
+      const value = event.target.value.toLowerCase();
+      const filtered = glyphs.filter((glyph) =>
+        glyph.props.glyph.name.toLowerCase().includes(value),
+      );
+
+      setFilteredGlyphs(filtered);
+    },
+    [glyphs],
+  );
 
   return (
     <FontGuard>
       <Layout fullscreen>
-        <div className="flex flex-col justify-between h-full">
-          <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
-            {currentGlyphs}
+        <div className="flex h-full flex-col items-center">
+          <div className="mb-4 w-full sm:w-96">
+            <Input placeholder={t("search")} onChange={handleSearch} />
+          </div>
+          <div className="grid w-full grow grid-cols-2 grid-rows-5 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
+            {displayedGlyphs}
           </div>
           {totalPages > 1 && (
             <div className="mt-6 pb-4">
               <GlyphsPagination
                 currentPage={currentPage}
                 totalPages={totalPages}
-                onPageChange={handlePageChange}
+                onPageChange={handleChangePage}
               />
             </div>
           )}
